6968228b57d41b4fac5731af1743f1eae3d804d1
Georgy Marrero <georgy.marrero@glidewelldental.com>
2016-07-29 15:51:24 -0700

    add files

---
+++ server.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+var http = require('http');
+var server = http.createServer(function(request, response) {});
+
+var portNumber = 8080;
+server.listen(portNumber, function() {
+    console.log((new Date()) + ' Server is listening on port ' + portNumber);
+});
+
+var WebSocketServer = require('websocket').server;
+wsServer = new WebSocketServer({
+    httpServer: server
+});
+
+wsServer.on('request', function(r){
+    // Code here to run on connection
+    var connection = r.accept('echo-protocol', r.origin);
+
+    // Specific id for this client & increment count
+    var id = count++;
+    // Store the connection method so we can loop through & contact all clients
+    clients[id] = connection
+
+    console.log((new Date()) + ' Connection accepted [' + id + ']');
+
+    // Create event listener
+    connection.on('message', function(message) {
+
+        // The string message that was sent to us
+        var msgString = message.utf8Data;
+        console.log("Received message", msgString);
+
+        // Loop through all clients
+        for(var i in clients){
+            // Send a message to the client with the message
+            clients[i].sendUTF(msgString);
+        }
+    });
+
+    connection.on('close', function(reasonCode, description) {
+        delete clients[id];
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+
+});
+
+var count = 0;
+var clients = {};
